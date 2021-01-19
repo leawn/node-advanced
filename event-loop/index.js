15f9f394b98701c3ff1ac6f1db833e9e2de7e97d
Leon <leonrubner2809@gmail.com>
2021-01-19 20:55:31 +0100

    finished with behavior of node clusters

--- event-loop/index.js
+++ event-loop/index.js
@@ -1,36 +1,20 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster');
+// I am a child, I am going to act like a server
+// and do nothing else
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+const { Worker } = require('worker_threads');
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-    // Cause index.js to be executed *again* but
-    // in child mode
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-} else {
-    // I am a child, I am going to act like a server
-    // and do nothing else
-    const express = require('express');
-    const crypto = require('crypto');
-    const app = express();
+app.get('/', (req, res, next) => {
+    const worker = new Worker(function () {
 
-    /*function doWork(duration) {
-        const start = Date.now();
-        while(Date.now() - start < duration) {}
-    }*/
-
-    app.get('/', (req, res, next) => {
-        /*doWork(5000);*/
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send('Hi there');
-        });
     });
+    worker.on('message', function() {});
+    worker.postMessage({});
+});
 
-    app.get('/fast', (req, res, next) => {
-        res.send('This was fast')
-    });
+app.get('/fast', (req, res, next) => {
+    res.send('This was fast');
+});
 
-    app.listen(3000);
-}
+app.listen(3000);
