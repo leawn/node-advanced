6bdc7cbc20d6c0b3f9f76fec9d2b6a961977708e
Leon <leonrubner2809@gmail.com>
2021-01-19 17:08:32 +0100

    behavior of node event loop commit

---
+++ event-loop/loop.js
@@ -0,0 +1,33 @@
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+// New timers, tasks, operations are recorded from myFile running
+myFile.runContents();
+
+function shouldContinue() {
+    // Check one: Any pending setTimeout, setInterval, setImmediate?
+    // Check two: Any pending OS Tasks? (Like server listening to port)
+    // Check three: Any pending long running operations? (Like fs module)
+    return pendingTimers.length || pendingOSTasks.length || pendingOperations;
+}
+
+// Entire body executes in one 'tick'
+while(shouldContinue()) {
+    // 1. Node looks at pendingTimers and sees if any functions
+    // are ready to be called (setTimeout, setInterval)
+
+    // 2. Node looks at pendingOSTasks and pendingOperations
+    // and calls relevant callbacks
+
+    // 3. Node pauses execution. Continue when...
+    // - a new pendingOSTask is done
+    // - a new pendingOperation is done
+    // - a timer is about to complete
+
+    // 4. Node looks at pendingTimers. Call any setImmediate
+
+    // 5. Handle any 'close' events
+}
+
+// Exit back to terminal
