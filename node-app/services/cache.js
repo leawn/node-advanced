bfe67693d372f47916756cc53c18bcd19604d796
Leon <leonrubner2809@gmail.com>
2021-01-21 21:15:43 +0100

    automatic cache cleaning commit

--- node-app/services/cache.js
+++ node-app/services/cache.js
@@ -5,16 +5,27 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+
+    return this; // making the method chainable
+}
+
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     }));
 
     // See if we have a value for a key in redis
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.hget(this.hashKey, key);
 
     // If we do, return that
     if (cacheValue) {
@@ -27,7 +38,13 @@ mongoose.Query.prototype.exec = async function () {
 
     // Otherwise, issue the query and store result in redis
     const result = await exec.apply(this, arguments);
-    client.set(key, JSON.stringify(result));
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
     return result;
+}
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    }
 }
